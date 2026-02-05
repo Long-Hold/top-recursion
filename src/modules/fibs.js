febf60850bf73ca4044ddbc946c9a68d29e2018a
Matthew <102175343+Long-Hold@users.noreply.github.com>
2026-02-04 19:06:17 -0500

    feat: Implement iterative fib sequence generator

--- src/modules/fibs.js
+++ src/modules/fibs.js
@@ -1,3 +1,11 @@
 export function fibs(num) {
-    
+    const sequence = [0, 1];
+    if (num === 1) return [0];
+    if (num === 2) return sequence;
+
+    for (let i = 1; i < num - 1; ++i) {
+        sequence.push(sequence[i] + sequence[i - 1]);
+    }
+
+    return sequence;
 }
--- src/tests/fibs.test.js
+++ src/tests/fibs.test.js
@@ -1,5 +1,28 @@
 import { fibs } from "../modules/fibs";
 
-test('Returns an array with the first number in sequence', () => {
-    expect(fibs(1)).toBe([1]);
-})
+describe('fibs()', () => {
+    test.each([
+        {
+            desc: 'the first value in sequence',
+            input: 1,
+            result: [0],
+        },
+        {
+            desc: 'the first 2 values in sequence',
+            input: 2,
+            result: [0, 1],
+        },
+        {
+            desc: 'the first 3 values in sequence',
+            input: 3,
+            result: [0, 1, 1],
+        },
+        { 
+            desc: 'the first 8 values in sequence',
+            input: 8,
+            result: [0, 1, 1, 2, 3, 5, 8, 13],
+        }
+    ])('returns an array with $desc', ({input, result}) => {
+        expect(fibs(input)).toStrictEqual(result);
+    })
+});
