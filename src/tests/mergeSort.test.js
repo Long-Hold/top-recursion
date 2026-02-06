72031ee13f74fe8aa27868eea09fa2494bd2d4c6
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-02-06 15:20:47 -0500

    feat: Add test cases for unsorted and sorted arrays

--- src/tests/mergeSort.test.js
+++ src/tests/mergeSort.test.js
@@ -5,6 +5,8 @@ describe('mergeSort()', () => {
         test.each([
             {input: [], result: []},
             {input: [1], result: [1]},
+            {input: [1,2,3,4,5], result: [1,2,3,4,5]},
+            {input: [3, 2, 1, 13, 8, 5, 0, 1], result: [0, 1, 1, 2, 3, 5, 8, 13]}
         ])('when passed $input, returns $result', ({input, result}) => {
             expect(mergeSort(input)).toStrictEqual(result);
         })
