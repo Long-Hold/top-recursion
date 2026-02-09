52536fded54b488afc1b108c4d597dc7beabb1f8
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-02-08 20:44:53 -0500

    feat: Add new test case for mergeSort function

--- src/tests/mergeSort.test.js
+++ src/tests/mergeSort.test.js
@@ -6,7 +6,8 @@ describe('mergeSort()', () => {
             {input: [], result: []},
             {input: [1], result: [1]},
             {input: [1,2,3,4,5], result: [1,2,3,4,5]},
-            {input: [3, 2, 1, 13, 8, 5, 0, 1], result: [0, 1, 1, 2, 3, 5, 8, 13]}
+            {input: [3, 2, 1, 13, 8, 5, 0, 1], result: [0, 1, 1, 2, 3, 5, 8, 13]},
+            {input: [105, 79, 100, 110], result: [79, 100, 105, 110]}
         ])('when passed $input, returns $result', ({input, result}) => {
             expect(mergeSort(input)).toStrictEqual(result);
         })
